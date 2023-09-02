fb3fe458609c50947d7e136545d80672d4ccc800
tofi1130 <taafuuu@gmail.com>
2023-09-02 10:01:27 +0500

    New feature, read file names to be operated on from another file

--- appendn.c
+++ appendn.c
@@ -1,24 +1,48 @@
-#include <stdio.h>
+#include "definitions.h"
+
+#include <getopt.h>
+
+
+struct option longoptions[] = {
+    {"files-names", required_argument, 0, 'f'},
+    {NULL, 0, NULL, '\0'}
+};
+
+
+static flag
+        aflag = 0,
+        f_switch = 0;
 
 
 int
 main (int argc, char **argv)
 {
-    if (argc > 1)
+    char optc,
+        *file_name;
+    
+    int optidx;
+
+    while ((optc = getopt_long(argc, argv, "f:", longoptions, &optidx)) != -1)
+    {
+        switch (optc)
+        {
+            case 'f':
+                aflag = f_switch = 1;
+                file_name = optarg;
+                break;
+        }
+    }
+
+    if (!aflag && argc > 1)
     {
         FILE *file = fopen(argv[1], "a+");
-        fseek(file, SEEK_SET, SEEK_END); // set cursor at end of file
 
-        long end = ftell(file);
-        fseek(file, end - 1, SEEK_SET);  // cursor at second last position
-
-        char character = fgetc(file); // get last character
-
-        if (character == '\n')
-            return 0;
-        else
+        if (!newline_at_end (file))
             fputc('\n', file);
     }
+
+    if (f_switch)
+        file_names_from_file (file_name);
     
     return 0;
 }
---
+++ definitions.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef unsigned int flag;
+
+int
+newline_at_end (FILE *fp)
+{
+    fseek(fp, SEEK_SET, SEEK_END); // set cursor at end of file
+
+    long end = ftell(fp);
+    fseek(fp, end - 1, SEEK_SET);  // cursor at second last position
+    
+    char character = fgetc(fp); // get last character
+
+    return (character == '\n');
+}
+
+
+/* read files names from a file and do operation on them */
+void
+file_names_from_file (char *entries_file)
+{
+    FILE *file = fopen(entries_file, "a+");
+    char file_name[255];
+    
+    while (1)
+    {
+        if (fscanf(file, "%s", file_name) != 1)
+            break;
+
+        else
+        {
+            if (!newline_at_end (file))
+                fputc('\n', file);
+        }
+
+        char file_name[255];
+    }
+}
