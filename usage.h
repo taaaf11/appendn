257a92cc2a5cb1fd005193dd673b05fb2a1f0d37
tofi1130 <taafuuu@gmail.com>
2023-09-04 20:18:12 +0500

    Fixed some typos

--- usage.h
+++ usage.h
@@ -7,7 +7,8 @@ usage (int status)
 {
     if (status != EXIT_SUCCESS)
     {
-        fprintf(stderr, "USAGE: appendn NAME_OF_FILE\n"
+        fprintf(stderr, "USAGE: appendn NAMES OF FILES\n"
+                        "  or   appendn NAME_OF_FILE\n"
                         "  or   appendn -f FILE\n\n");
 
         fprintf(stderr, "    --files-names,  -f FILE\t\tGet files' names to be operated on from FILE.\n"
@@ -17,7 +18,8 @@ usage (int status)
         exit(status);
     }
 
-    fprintf(stdout, "USAGE: appendn NAME_OF_FILE\n"
+    fprintf(stdout, "USAGE: appendn NAMES OF FILES\n"
+                    "  or   appendn NAME_OF_FILE\n"
                     "  or   appendn -f FILE\n\n");
                         
     fprintf(stdout, "    --files-names,  -f FILE\t\tGet files' names to be operated on from FILE.\n"
